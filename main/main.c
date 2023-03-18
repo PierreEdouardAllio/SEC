ace3c8250e1633c0839e1b42c51578284862fb1e
PierreEdouardAllio <117344210+PierreEdouardAllio@users.noreply.github.com>
2023-03-18 15:00:24 +0100

    First working version of the time management
    This files contain a working example of the time management functionalities.
    Remember to change the WiFi network SSID and Password in the configuration menu !

---
+++ main/main.c
@@ -0,0 +1,20 @@
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+
+//FreeRTOS variables//
+extern TaskHandle_t xWiFi_Connect_SNTP;
+extern TaskHandle_t xGet_RTC_Time;
+//Tasks included in TimeManagement.c"
+void WiFi_Connect_SNTP(void);
+void Get_RTC_Time(void);
+
+void app_main(void)
+{
+	//Create Task//
+	xTaskCreatePinnedToCore(&WiFi_Connect_SNTP, "WiFi_Connect_SNTP", 10000, NULL, 5,&xWiFi_Connect_SNTP,0);
+	vTaskDelay(5000/portTICK_PERIOD_MS);
+	xTaskCreatePinnedToCore(&Get_RTC_Time,"Get_RTC_Time",1000,NULL,2,&xGet_RTC_Time,0);
+	vTaskDelay(1000/portTICK_PERIOD_MS);
+}
