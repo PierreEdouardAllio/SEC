c7f568e6d1984c612cfa7b79acb02923115bd6fc
PierreEdouardAllio <117344210+PierreEdouardAllio@users.noreply.github.com>
2023-03-21 11:39:32 +0100

    Added header file and corrected tasks

--- main/TimeManagement.c
+++ main/TimeManagement.c
@@ -67,106 +67,113 @@ time_t currenttime;
 //FreeRTOS variables//
 TaskHandle_t xWiFi_Connect_SNTP=NULL;
 TaskHandle_t xGet_RTC_Time=NULL;
-TaskHandle_t xSleep_Mode=NULL;
 //###################################################################################################
 //#######################  					FONCTIONS 					 ############################
 //###################################################################################################
 /*
- * This function connects to a NTP server using the WiFi network previously
- * configured in the configuration menu. Function is supposed to be used with FreeRTOS.
+ * This task connects to a NTP server using the WiFi network previously
+ * configured in the configuration menu.
  * This task connects to the WiFi network, retrieves the time from the NTP server,
  * synchronizes the RTC to the time retrieved and disconnects from WiFi network.
  */
 void WiFi_Connect_SNTP(void)
 {
-    //1-Initialize NVS//
-    nvs_flash_init();
-    //2-Initialize TCP-IP//
-    esp_netif_init();
-    esp_event_loop_create_default();
-    ESP_LOGI(TAG, "WiFi prerequisites activated");
-    //3-Configure WiFi Station//
-    esp_netif_create_default_wifi_sta();
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    //4-Initialize WiFi//
-    esp_wifi_init(&cfg);
-    //5-Configure WiFi//
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = EXAMPLE_ESP_WIFI_SSID,
-			.password = EXAMPLE_ESP_WIFI_PASS,
-            .threshold.authmode = ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD,
-            .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
-        },
-    };
-    esp_wifi_set_mode(WIFI_MODE_STA);
-    esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
-    ESP_LOGI(TAG, "WiFi configuration done");
-    //6-Start WiFi Connection//
-    esp_wifi_start();
-    ESP_LOGI(TAG, "WiFi turned on");
-    //6-8-10-Try to Connect to WiFi//
-	while ((esp_wifi_connect()!=ESP_OK)&&(Number_Tries<EXAMPLE_ESP_MAXIMUM_RETRY))
+	while(1)
 	{
-		Number_Tries++;
-		ESP_LOGI(TAG, "Connecting to WiFi network please wait...");
-	}
-	//7-Suspend Task Unable to Connect to WiFi//
-	if(Number_Tries==EXAMPLE_ESP_MAXIMUM_RETRY)
-	{
-		ESP_LOGI(TAG, "Failed to connect to WiFi network -> suspending WiFi task");
+	   //1-Initialize NVS//
+		nvs_flash_init();
+		//2-Initialize TCP-IP//
+		esp_netif_init();
+		esp_event_loop_create_default();
+		ESP_LOGI(TAG, "WiFi prerequisites activated");
+		//3-Configure WiFi Station//
+		esp_netif_create_default_wifi_sta();
+		wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+		//4-Initialize WiFi//
+		esp_wifi_init(&cfg);
+		//5-Configure WiFi//
+		wifi_config_t wifi_config = {
+			.sta = {
+				.ssid = EXAMPLE_ESP_WIFI_SSID,
+				.password = EXAMPLE_ESP_WIFI_PASS,
+				.threshold.authmode = ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD,
+				.sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
+			},
+		};
+		esp_wifi_set_mode(WIFI_MODE_STA);
+		esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
+		ESP_LOGI(TAG, "WiFi configuration done");
+		//6-Start WiFi Connection//
+		esp_wifi_start();
+		ESP_LOGI(TAG, "WiFi turned on");
+		//6-8-10-Try to Connect to WiFi//
+		while ((esp_wifi_connect()!=ESP_OK)&&(Number_Tries<EXAMPLE_ESP_MAXIMUM_RETRY))
+		{
+			Number_Tries++;
+			ESP_LOGI(TAG, "Connecting to WiFi network please wait...");
+		}
+		//7-Suspend Task Unable to Connect to WiFi//
+		if(Number_Tries==EXAMPLE_ESP_MAXIMUM_RETRY)
+		{
+			ESP_LOGI(TAG, "Failed to connect to WiFi network -> suspending WiFi task");
+			vTaskSuspend(xWiFi_Connect_SNTP);
+		}
+		ESP_LOGI(TAG, "Successfully connected to WiFi network");
+		//11-Initialize the SNTP service//
+		sntp_setoperatingmode(SNTP_OPMODE_POLL);
+		sntp_setservername(0, NTP_SERVER_NAME);
+		//12-Connect to the SNTP service//
+		sntp_init();
+		ESP_LOGI(TAG, "Connected to NTP server");
+		// wait for the service to set the time
+		time_t now;
+		struct tm timeinfo;
+		//System clock time as time_t//
+		time(&now);
+		//Converted system time as tm//
+		localtime_r(&now, &timeinfo);
+		//As long as RTC doesnt have good value//
+		while(timeinfo.tm_year < (CURRENT_YEAR - 1900))
+		{
+			ESP_LOGI(TAG, "Waiting for SNTP to respond please wait...");
+			vTaskDelay(1000 / portTICK_PERIOD_MS);
+			//Getting RTC time//
+			time(&now);
+			localtime_r(&now, &timeinfo);
+		}
+		ESP_LOGI(TAG, "Time has been acquired from SNTP service");
+		//14-Stop WiFi Connection//
+		esp_wifi_disconnect();
+		ESP_LOGI(TAG, "Successfully disconnected from WiFi network");
+		//15-Turn OFF WiFi//
+		esp_wifi_stop();
+		ESP_LOGI(TAG, "WiFi turned off");
+		//16-Task Finished//
+		ESP_LOGI(TAG, "Suspending WiFi task");
 		vTaskSuspend(xWiFi_Connect_SNTP);
 	}
-	ESP_LOGI(TAG, "Successfully connected to WiFi network");
-    //11-Initialize the SNTP service//
-	sntp_setoperatingmode(SNTP_OPMODE_POLL);
-	sntp_setservername(0, NTP_SERVER_NAME);
-	//12-Connect to the SNTP service//
-	sntp_init();
-	ESP_LOGI(TAG, "Connected to NTP server");
-	// wait for the service to set the time
-	time_t now;
-	struct tm timeinfo;
-	//System clock time as time_t//
-	time(&now);
-	//Converted system time as tm//
-	localtime_r(&now, &timeinfo);
-	//As long as RTC doesnt have good value//
-	while(timeinfo.tm_year < (CURRENT_YEAR - 1900))
-	{
-		ESP_LOGI(TAG, "Waiting for SNTP to respond please wait...");
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
-		//Getting RTC time//
-		time(&now);
-		localtime_r(&now, &timeinfo);
-	}
-	ESP_LOGI(TAG, "Time has been acquired from SNTP service");
-	//14-Stop WiFi Connection//
-    esp_wifi_disconnect();
-    ESP_LOGI(TAG, "Successfully disconnected from WiFi network");
-    //15-Turn OFF WiFi//
-    esp_wifi_stop();
-    ESP_LOGI(TAG, "WiFi turned off");
-    //16-Task Finished//
-    ESP_LOGI(TAG, "Suspending WiFi task");
-    vTaskSuspend(xWiFi_Connect_SNTP);
+
 }
 /*
- * This function updates the time storage variable "timedata" with the RTC time.
- * This function is supposed to be used with FreeRTOS.
+ * This task updates the time storage variable "timedata" with the RTC time.
  */
 void Get_RTC_Time(void)
 {
-	//Get current RTC time in time_t//
-	time(&currenttime);
-	//Transform RTC time into tm struct//
-	localtime_r(&currenttime,&timedata);
-	//Display (https://www.tutorialspoint.com/c_standard_library/time_h.htm)//
-	ESP_LOGI(TAG,"Time : %d : %d : %d",timedata.tm_hour,timedata.tm_min,timedata.tm_sec);
-	ESP_LOGI(TAG,"Date : %d : %d : %d",timedata.tm_mday,1+timedata.tm_mon,1900+timedata.tm_year);
-	vTaskSuspend(xGet_RTC_Time);
+	while(1)
+	{
+		//Get current RTC time in time_t//
+		time(&currenttime);
+		//Transform RTC time into tm struct//
+		localtime_r(&currenttime,&timedata);
+		//Display (https://www.tutorialspoint.com/c_standard_library/time_h.htm)//
+		ESP_LOGI(TAG,"Time : %d : %d : %d",timedata.tm_hour,timedata.tm_min,timedata.tm_sec);
+		ESP_LOGI(TAG,"Date : %d : %d : %d",timedata.tm_mday,1+timedata.tm_mon,1900+timedata.tm_year);
+		vTaskSuspend(xGet_RTC_Time);
+	}
 }
-
+/*
+ * This function activates all the wakeup sources and activates light sleep mode.
+ */
 void Sleep_Mode(void)
 {
 	ESP_LOGI(TAG,"Going into sleep mode...");
@@ -178,5 +185,4 @@ void Sleep_Mode(void)
 	esp_light_sleep_start();
 	esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_ALL);
 	ESP_LOGI(TAG,"Sleep mode deactivated");
-	vTaskSuspend(xSleep_Mode);
 }
--- main/main.c
+++ main/main.c
@@ -2,15 +2,10 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
-
+#include "TimeManagement.h"
 //FreeRTOS variables//
 extern TaskHandle_t xWiFi_Connect_SNTP;
 extern TaskHandle_t xGet_RTC_Time;
-extern TaskHandle_t xSleep_Mode;
-//Tasks included in TimeManagement.c"
-void WiFi_Connect_SNTP(void);
-void Get_RTC_Time(void);
-void Sleep_Mode(void);
 
 void app_main(void)
 {
@@ -18,7 +13,5 @@ void app_main(void)
 	xTaskCreatePinnedToCore(&WiFi_Connect_SNTP, "WiFi_Connect_SNTP", 10000, NULL,1,&xWiFi_Connect_SNTP,0);
 	vTaskDelay(5000/portTICK_PERIOD_MS);
 	xTaskCreatePinnedToCore(&Get_RTC_Time,"Get_RTC_Time",1000,NULL,3,&xGet_RTC_Time,0);
-	vTaskDelay(5000/portTICK_PERIOD_MS);
-	xTaskCreatePinnedToCore(&Sleep_Mode,"Sleep_Mode",1000,NULL,5,&xSleep_Mode,0);
 	vTaskDelay(1000/portTICK_PERIOD_MS);
 }
